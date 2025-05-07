406b13b194b30bb12504aa2edf90b5763a312058
rupam office <rupamdhara.1579@gmail.com>
2025-05-07 18:53:11 +0530

    Updates

--- affored/settings.py
+++ affored/settings.py
@@ -147,12 +147,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files (Uploaded files)
 # https://docs.djangoproject.com/en/5.2/ref/settings/#media-root

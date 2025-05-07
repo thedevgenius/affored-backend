3c9ecc50c6d6ed8f47171ab3cd23b0ebc3629358
rupam office <rupamdhara.1579@gmail.com>
2025-05-07 18:56:33 +0530

    Updates

--- affored/settings.py
+++ affored/settings.py
@@ -150,9 +150,9 @@ USE_TZ = True
 import os
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
